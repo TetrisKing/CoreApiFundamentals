8b8dda85cc70a84c10344c6028ce9de0b6c0d9a3
Peter Geddis <peter.geddis@cubiks.com>
2019-05-09 15:35:53 +0100

    C3-L5

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        private readonly ICampRepository repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await repository.GetAllCampsAsync();
+                return Ok(results);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
