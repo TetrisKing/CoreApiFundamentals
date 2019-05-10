cb012b479232a5c0d0b93726a202b022814db8ce
Peter Geddis <peter.geddis@cubiks.com>
2019-05-10 15:32:08 +0100

    C3-L8

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,19 +14,37 @@ namespace CoreCodeCamp.Controllers
     public class CampsController : Controller
     {
         private readonly ICampRepository repository;
+        private readonly IMapper mapper;
 
-        public CampsController(ICampRepository repository)
+        public CampsController(ICampRepository repository, IMapper mapper)
         {
             this.repository = repository;
+            this.mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<CampModel[]>> Get()
         {
             try
             {
                 var results = await repository.GetAllCampsAsync();
-                return Ok(results);
+                return mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var results = await repository.GetCampAsync(moniker);
+                if (results == null)
+                    return NotFound();
+                return mapper.Map<CampModel>(results);
             }
             catch (Exception e)
             {
